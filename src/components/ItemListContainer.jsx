14f78a32159794ad5b6b91202506185af1b8f33e
maxiferrioli <maxiferrioli.97@outlook.es>
2021-12-16 17:40:00 -0300

    cambios

---
+++ src/components/ItemListContainer.jsx
@@ -0,0 +1,24 @@
+import { useState, useEffect } from "react";
+import {getFetch} from "../helpers/getFetch"
+import ItemList from "./ItemList";
+
+function ItemListContainer({ greeting }) {
+  const [productos, setProductos] = useState([]);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    getFetch
+      .then((resp) => setProductos(resp))
+      .catch((err) => console.log(err))
+      .finally(() => setLoading(false));
+  }, []);
+
+  return (
+    <>
+      {greeting}
+      {loading ? <h2>Cargando...</h2> : <ItemList productos={productos}/>}
+    </>
+  );
+}
+
+export default ItemListContainer;
