45b58235c8ebc2c8d2920b3131c4681767acc7d4
maxiferrioli <maxiferrioli.97@outlook.es>
2021-12-17 00:18:17 -0300

    entrega 1

--- src/App.js
+++ src/App.js
@@ -1,18 +1,45 @@
-import './App.css';
-import ItemListContainer from './components/ItemListContainer';
-import { Navbar } from './components/NavBar';
+import "./App.css";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import {Link} from 'react-router-dom'
+import ItemDetailContainer from "./components/ItemDetailContainer";
+import ItemListContainer from "./components/ItemListContainer";
+import { Navbar } from "./pages/NavBar";
+import { Footer } from "./pages/Footer";
 
 function App() {
   return (
-    <div>
-      <header>
-        <Navbar/>
-      </header>
-      <main>
-        <ItemListContainer/>
-      </main>
-      
-    </div>
+    <BrowserRouter>
+      <div>
+        <Navbar />
+        <Routes>
+          <Route
+            exact
+            path="/"
+            element={
+              <ItemListContainer greeting="hola soy ItemListContainer" />
+            }
+          />
+          <Route
+            exact
+            path="/categoria"
+            element={<ItemListContainer greeting="hola soy" />}
+          />
+          <Route
+            exact
+            path="/detalle"
+            element={
+              <ItemDetailContainer greeting="hola soy ItemDetailContainer" />
+            }
+          />
+          <Route
+            exact
+            path="/carrito"
+            element={<ItemListContainer greeting="hola soy" />}
+          />
+        </Routes>
+        <Footer />
+      </div>
+    </BrowserRouter>
   );
 }
 
